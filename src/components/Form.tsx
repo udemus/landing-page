f10cadc119a9715160d09df00a0ba0376f909096
zbeyens <ziyad.beyens@gmail.com>
2021-07-13 18:06:32 +0200

    styling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 
 export const Form = () => (
   <div
-    className="ml-form-embed"
+    className="ml-form-embed min-h-[83px]"
     data-account="3292648:j0b6l7d2p0"
     data-form="4395622:f7b6d5"
     css={css`
