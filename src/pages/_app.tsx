736f25a0b687ce85ac1519122d8aa00d6cfcd5ae
zbeyens <ziyad.beyens@gmail.com>
2021-07-13 13:38:35 +0200

    ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,30 @@
-import { AppProps } from 'next/app';
-
 import '../styles/main.css';
+import { useEffect } from 'react';
+import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { GA_TRACKING_ID, pageview } from '../lib/gtag';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      if (!GA_TRACKING_ID) return;
+
+      pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Component {...pageProps} />
+  );
+};
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,13 @@
-// This code prevents a Flash Of Unstyled Content (FOUC)
-// on load. Without it, the styles are only added once
-// react loads on the frontend
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+/* eslint-disable react/no-danger */
+import Document, { Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { GA_TRACKING_ID } from '../lib/gtag';
 import { AppConfig } from '../utils/AppConfig';
 
 export default class MyDocument extends Document {
+  // This code prevents a Flash Of Unstyled Content (FOUC)
+  // on load. Without it, the styles are only added once
+  // react loads on the frontend
   static async getInitialProps(ctx: any) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -34,7 +36,25 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        {GA_TRACKING_ID && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', { page_path: window.location.pathname });
+                `,
+              }}
+            />
+          </>
+        )}
         <body>
           <Main />
           <NextScript />
